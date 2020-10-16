660fd3219d2a44e430216b7cbe8e01eb4dc9a294
angeralr <56032043+angeralr@users.noreply.github.com>
2020-10-16 14:32:26 +0400

    Add files via upload

---
+++ greeting.go
@@ -0,0 +1,15 @@
+package greeting
+
+import (
+"fmt"
+"github.com/angeralr/greeting"
+"github.com/angeralr/greeting/dansk"
+"github.com/angeralr/greeting/deutsch"
+
+func Hello() {
+	fmt.Println("Hello!")
+}
+
+func Hi() {
+	fmt.Println("Hi!")
+}
